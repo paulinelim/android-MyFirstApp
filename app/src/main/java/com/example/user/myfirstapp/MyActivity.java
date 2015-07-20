384860c10b6b719b7b4204679e9e126bff52a9e1
Pauline Lim <paulinelim92@gmail.com>
2015-07-21 01:04:12 +0800

    Edited for Starting Another Activity

--- app/src/main/java/com/example/user/myfirstapp/MyActivity.java
+++ app/src/main/java/com/example/user/myfirstapp/MyActivity.java
@@ -1,12 +1,18 @@
 package com.example.user.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MyActivity extends ActionBarActivity {
+
+    // make sure keys are unique!
+    public final static String EXTRA_MESSAGE = "com.example.user.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,4 +41,15 @@ public class MyActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /** Called when the user clicks the send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message); // key-value pair
+        startActivity(intent);
+    }
+
+
 }
